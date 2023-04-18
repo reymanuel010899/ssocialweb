988a054688525d922e1bcdaec7899f2c645d483d
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-18 18:28:20 -0400

    first commit

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (SolisitudMOdel, AmigoModels, ChatModels, PostModel, LikeModels, ComentarModels , ConpartirModels,
+                     NotificacionesModels,ComentarioLike,
+                     ChatVistoModels   )
 
 # Register your models here.
+admin.site.register(SolisitudMOdel)
+admin.site.register(AmigoModels)
+admin.site.register(PostModel)
+admin.site.register(LikeModels)
+admin.site.register(ComentarModels)
+admin.site.register(ConpartirModels)
+admin.site.register(NotificacionesModels)
+admin.site.register(ChatModels)
+admin.site.register(ComentarioLike)
+admin.site.register(ChatVistoModels)
