868852a4352bb264ebef0641fa68cea90453dce4
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-18 21:00:43 -0400

    12

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -30,7 +30,7 @@ import json
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-)jsml3k&%k%w+z0orm=@(vzijp&12na!93mc_b9_x2-_-v8+9j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -148,8 +148,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER =  "reymanuelferrera78@gmail.com"
+EMAIL_HOST_PASSWORD = "cuorhvnxgacyvvih"
 EMAIL_USE_TLS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
    
