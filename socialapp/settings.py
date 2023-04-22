9f98a3ff40b012e0dd5ac2134e6a5833148e15e0
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-22 15:54:44 -0400

    4

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -161,8 +161,8 @@ STATICFILES_DIRS = [
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  'reymanuelferrera78@gmail.com'
-EMAIL_HOST_PASSWORD = 'eednzsuhokmsdjuo'
+EMAIL_HOST_USER =  'socialkiptly@gmail.com'
+EMAIL_HOST_PASSWORD = 'izqqdqgpwyivdxfq'
 EMAIL_USE_TLS = True
 
    
